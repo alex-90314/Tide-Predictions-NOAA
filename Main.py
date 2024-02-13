326194ec246b7e5be0b45137b69a30e1cbcb12c3
Alexander Sando <boricua422012+github@gmail.com>
2024-02-12 21:40:01 -0500

    First upload

---
+++ Main.py
@@ -0,0 +1,7 @@
+# import csv
+
+# with open('Tide_Info.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     for row in spamreader:
+#         print(', '.join(row))
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tide Predictions (NOAA)
  
+This is a small project that will request the current tide info and future tide predictions from the Virginia Key, Biscayne Bay (ID: 8723214) NOAA Station in Miami, Florida.
