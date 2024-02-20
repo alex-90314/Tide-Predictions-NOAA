c37edd8108a0ab4c6755aba846593a32e4d3179d
Alexander Sando <boricua422012+github@gmail.com>
2024-02-20 16:20:57 -0500

    Attempting a cleaner output
    Started working on a cleaner way of outputting the CSV file that will be auto-created by NOAA

--- Main.java
+++
@@ -1,7 +0,0 @@
-import java.util.stream.Collectors;
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-  }
--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+#old method - reliant on the csv module
+
 # import csv
 
 # with open('Tide_Info.csv', newline='') as csvfile:
@@ -5,3 +7,8 @@
 #     for row in spamreader:
 #         print(', '.join(row))
 
+# new method - not reliant on the csv module
+with open('Tide_Info.csv') as csv:
+    for line in csv:
+        Predict = line
+        Print(f"On {Date} @ {Time} the tide will be {Predict}")
